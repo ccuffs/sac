94e469824f8f95067a9370751949c06bde5f3379
Mateus Koppe <mateus.koppe@gmail.com>
2019-08-16 21:48:50 -0300

    Improvments in navbar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
-var menu= document.getElementById("menu")
-var btn= document.getElementById("button")
-btn.addEventListener('click',function(){
-    menu.classList.toggle('menu-mobile__open').style.transition="0.4s ease"
+var menuMobile = document.querySelector(".menu-mobile")
+var toggleButton = document.querySelector(".navbar__toggle-button");
+
+toggleButton.addEventListener('click', function () {
+    menuMobile.classList.toggle('menu-mobile--opened')
 })
